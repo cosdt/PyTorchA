4c2413d18f6de2e1e99ecce4bafce4608acc842e
orangeH25 <18085625039@163.com>
2026-08-17 11:22:40 +0000

    support hif8 train

---
+++ torch_npu/utils/hifloat8_train/hifloat8_linear.py
@@ -0,0 +1,119 @@
+import torch
+from torch import nn
+
+from typing import Callable, Optional
+
+from torch_npu.utils.hifloat8_train.hifloat8_training_tensor import hp_tensor_to_hifloat8
+
+@torch._dynamo.allow_in_graph
+class matmul_with_hifloat8(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input: torch.Tensor, weight: torch.Tensor):
+        ctx.save_for_backward(input, weight)
+
+        input_hif8 = hp_tensor_to_hifloat8(input)
+        weight_hif8 = hp_tensor_to_hifloat8(weight)
+
+        output = torch.mm(input_hif8, weight_hif8)
+        return output
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        input, weight = ctx.saved_tensors
+
+        input_hif8 = hp_tensor_to_hifloat8(input)
+        weight_hif8 = hp_tensor_to_hifloat8(weight)
+        grad_output_hif8 = hp_tensor_to_hifloat8(grad_output)
+
+        grad_input = torch.mm(grad_output_hif8, weight_hif8.t())
+        grad_weight = torch.mm(input_hif8.t(), grad_output_hif8)
+
+        return grad_input, grad_weight
+
+
+
+class HiFloat8Linear(torch.nn.Linear):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        output = matmul_with_hifloat8.apply(input, self.weight.t())
+        if self.bias is not None:
+            output = output + self.bias.to(output.dtype)
+        return output
+
+    @classmethod
+    def from_float(cls, mod: torch.nn.Linear):
+        with torch.device("meta"):
+            new_mod = cls(mod.in_features, mod.out_features, bias=False)
+        new_mod.weight = mod.weight
+        new_mod.bias = mod.bias
+        return new_mod
+
+
+
+def swap_linear_layers(
+    module: nn.Module,
+    from_float_func: Callable[[nn.Linear], nn.Linear],
+    *,
+    module_filter_fn: Optional[Callable[[nn.Module, str], bool]] = None,
+) -> nn.Module:
+    if isinstance(module, nn.Linear) and (
+        module_filter_fn is None or module_filter_fn(module, "")
+    ):
+        if len(list(module.children())) > 0:
+            raise AssertionError(
+                f"Does not support a root nn.Linear with children: {module}"
+            )
+        return from_float_func(
+            module,
+        )
+
+    root_module = module
+
+    def post_order_traversal(
+        module: nn.Module,
+        cur_fqn: Optional[str] = None,
+        parent_module: Optional[nn.Module] = None,
+    ):
+        if cur_fqn is None:
+            cur_fqn = ""
+
+        for child_module_name, child_module in module.named_children():
+            if cur_fqn == "":
+                new_fqn = child_module_name
+            else:
+                new_fqn = f"{cur_fqn}.{child_module_name}"
+
+            post_order_traversal(child_module, new_fqn, module)
+
+        if isinstance(module, nn.Linear) and (
+            module_filter_fn is None or module_filter_fn(module, cur_fqn)
+        ):
+            assert parent_module is not None, (
+                f"Linear root module should return early: {module}"
+            )
+            new_linear_module = from_float_func(module)
+            cur_module_name = cur_fqn.split(".")[-1]
+            setattr(parent_module, cur_module_name, new_linear_module)
+
+    post_order_traversal(root_module)
+    return root_module
+
+
+def convert_to_hifloat8_training(
+    module: nn.Module,
+    *,
+    module_filter_fn: Optional[Callable[[nn.Module, str], bool]] = None,
+) -> nn.Module:
+
+    torch._C._log_api_usage_once("...")
+
+    def from_float(m: nn.Linear) -> HiFloat8Linear:
+        return HiFloat8Linear.from_float(m)
+
+    return swap_linear_layers(
+        module,
+        from_float,
+        module_filter_fn=module_filter_fn,
+    )
---
+++ torch_npu/utils/hifloat8_train/hifloat8_training_tensor.py
@@ -0,0 +1,139 @@
+
+import torch
+import torch_npu
+
+from torch.distributed._tensor import DTensor
+from typing import Dict
+
+
+tex = torch_npu._C._cd
+aten = torch.ops.aten
+
+NPU_CUSTOM_DType = {
+    torch.uint8: tex.DType.uint8,
+    torch.int32: tex.DType.int32,
+    torch.float32: tex.DType.float32,
+    torch.half: tex.DType.float16,
+    torch.bfloat16: tex.DType.bfloat16,
+}
+
+# @torch._dynamo.allow_in_graph
+class _ToHiFloat8ConstrFunc(torch.autograd.Function):
+
+    @staticmethod
+    @torch._dynamo.disable
+    def forward(
+        ctx,
+        input: torch.Tensor,
+    ):
+        if isinstance(input, DTensor):
+            input = input.to_local()
+        
+        input = input.contiguous().npu().detach()
+        if input.dtype not in (torch.float32, torch.bfloat16, torch.float16):
+            input = input.float()
+
+        # Cast data to HIF8
+        data = tex.cast_to_fp8(
+            input.view(1, -1),
+            tex.DType.hifloat8,
+        )
+        data = data.view(input.size())
+
+        # Construct HIF8 tensor
+        return HiFloat8TrainingTensor(
+            data=data,
+            orig_dtype=input.dtype,
+        )
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        return grad_output
+
+
+# @torch._dynamo.allow_in_graph
+class _FromHiFloat8ConstrFunc(torch.autograd.Function):
+
+    @staticmethod
+    def forward(
+        ctx,
+        input: torch.Tensor,
+    ):
+        data = input._data.contiguous().view(1, -1).detach()
+        out = tex.cast_from_fp8(
+            data,
+            tex.DType.hifloat8,
+            NPU_CUSTOM_DType[input._orig_dtype],
+        )
+        out = out.view(input.size())
+        return out
+
+
+    @staticmethod
+    def backward(ctx, grad_output: torch.Tensor):
+        return grad_output
+
+
+class HiFloat8TrainingTensor(torch.Tensor):
+    _data: torch.Tensor
+    _orig_dtype: torch.dtype
+    __slots__ = ["_data", "_orig_dtype"]
+
+    def __new__(
+        cls,
+        data: torch.Tensor,
+        orig_dtype: torch.dtype,
+    ):
+        self = torch.Tensor._make_wrapper_subclass(
+            cls,
+            data.size(),
+            strides=data.stride(),
+            storage_offset=data.storage_offset(),
+            dtype=orig_dtype,
+            layout=data.layout,
+            requires_grad=data.requires_grad,
+            device=data.device,
+        )
+        self._data = data
+        self._orig_dtype = orig_dtype
+
+        return self
+
+    def __repr__(self):
+        return f"HiFloat8TrainingTensor({self._data}, orig_dtype={self._orig_dtype})"
+
+    def __tensor_flatten__(self):
+        return ["_data"], {"_orig_dtype": self._orig_dtype}
+
+    @staticmethod
+    def __tensor_unflatten__(tensor_dict: Dict, metadata, outer_size, outer_stride):
+        return HiFloat8TrainingTensor(tensor_dict["_data"], metadata["_orig_dtype"])
+
+    def to_original_precision(self):
+        return _FromHiFloat8ConstrFunc.apply(self)
+
+    @classmethod
+    def __torch_dispatch__(cls, func, types, args, kwargs=None):
+        from torch_npu.utils.hifloat8_train.hifloat8_ops import HIFLOAT8_OPS_TABLE
+
+        def allowed_subclasses(type):
+            return (
+                issubclass(cls, type)
+                or issubclass(torch._subclasses.fake_tensor.FakeTensor, type)
+                or issubclass(
+                    torch._subclasses.functional_tensor.FunctionalTensor, type
+                )
+            )
+
+        if not all(allowed_subclasses(t) for t in types):
+            return NotImplemented
+
+        if func in HIFLOAT8_OPS_TABLE:
+            return HIFLOAT8_OPS_TABLE[func](func, args, kwargs)
+        raise NotImplementedError(f"attempting to run {func}, this is not supported")
+
+    __torch_function__ = torch._C._disabled_torch_function_impl
+
+
+def hp_tensor_to_hifloat8(input: torch.Tensor) -> HiFloat8TrainingTensor:
+    return _ToHiFloat8ConstrFunc.apply(input)
